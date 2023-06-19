85a87fe31fbbe797a34f373b0012c3221471f137
JasonXenon <jason.levecq@ifosup.wavre.be>
2023-06-19 22:24:03 +0200

    update modern js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Récupérer les éléments HTML du quiz
-var questionElement = document.getElementById("question");
-var reponsesElement = document.getElementById("reponses");
+// Sélection des éléments HTML du quiz
+const questionElement = document.getElementById("question");
+const reponsesElement = document.getElementById("reponses");
 
 // Données du quiz (à titre d'exemple)
-var quizData = {
+const quizData = {
   questions: [
     {
-      question: "Iana est-elle une assaillant ou défenseur ?",
-      reponses: ["Assaillant", "défenseur"],
+      question: "Iana est-elle une assaillante ou une défenseuse ?",
+      reponses: ["Assaillante", "Défenseuse"],
       reponseCorrecte: 0
     },
     {
@@ -16,26 +16,26 @@ var quizData = {
       reponseCorrecte: 0
     },
     {
-        question: "Combien de caméra à Valkyrie ?",
-        reponses: ["4", "3", "5", "2"],
-        reponseCorrecte: 1
+      question: "Combien de caméras possède Valkyrie ?",
+      reponses: ["4", "3", "5", "2"],
+      reponseCorrecte: 1
     },
     {
-        question: "Quel est l'escouade de Brava ?",
-        reponses: ["Redhammer", "Nighthaven", "Viperstrike"],
-        reponseCorrecte: 2
+      question: "Quel est l'escouade de Brava ?",
+      reponses: ["Redhammer", "Nighthaven", "Viperstrike"],
+      reponseCorrecte: 2
     },
     {
-        question: "Quel est l'alternative à thermite le plus efficace pour faire des breach lourdes de loin ?",
-        reponses: ["Zero", "Osa", "Maverick", "Ace"],
-        reponseCorrecte: 3
+      question: "Quel est l'alternative à Thermite la plus efficace pour créer des breaches lourdes à distance ?",
+      reponses: ["Zero", "Osa", "Maverick", "Ace"],
+      reponseCorrecte: 3
     }
   ]
 };
 
 // Variables pour suivre l'état du quiz
-var questionIndex = 0;
-var score = 0; // Variable pour stocker le score
+let questionIndex = 0;
+let score = 0; // Variable pour stocker le score
 
 // Fonction pour générer et afficher une question
 function afficherQuestion(question) {
@@ -45,29 +45,26 @@ function afficherQuestion(question) {
   reponsesElement.innerHTML = "";
 
   // Générer les boutons de réponse
-  for (var i = 0; i < question.reponses.length; i++) {
-    var reponse = question.reponses[i];
-    var bouton = document.createElement("button");
+  question.reponses.forEach((reponse, index) => {
+    const bouton = document.createElement("button");
     bouton.textContent = reponse;
-    bouton.addEventListener("click", (function(index) {
-      return function() {
-        validerReponse(index);
-      };
-    })(i));
+    bouton.addEventListener("click", () => {
+      validerReponse(index);
+    });
     reponsesElement.appendChild(bouton);
-  }
+  });
 }
 
 // Fonction pour valider la réponse et passer à la question suivante
 function validerReponse(indexReponse) {
-  var question = quizData.questions[questionIndex];
-  var reponseCorrecte = question.reponseCorrecte;
-  var boutonsReponses = reponsesElement.getElementsByTagName("button");
+  const question = quizData.questions[questionIndex];
+  const reponseCorrecte = question.reponseCorrecte;
+  const boutonsReponses = reponsesElement.getElementsByTagName("button");
 
   // Désactiver les boutons de réponse pour empêcher de sélectionner une autre réponse
-  for (var i = 0; i < boutonsReponses.length; i++) {
-    boutonsReponses[i].disabled = true;
-  }
+  Array.from(boutonsReponses).forEach(bouton => {
+    bouton.disabled = true;
+  });
 
   // Vérifier la réponse choisie
   if (indexReponse === reponseCorrecte) {
@@ -75,14 +72,13 @@ function validerReponse(indexReponse) {
   }
 
   // Délai d'attente en millisecondes avant de passer à la question suivante
-  var delaiAttente = 2000;
+  const delaiAttente = 2000;
 
   // Passer à la question suivante après le délai d'attente
-  setTimeout(function() {
+  setTimeout(() => {
     questionIndex++;
-
     if (questionIndex < quizData.questions.length) {
-      var prochaineQuestion = quizData.questions[questionIndex];
+      const prochaineQuestion = quizData.questions[questionIndex];
       afficherQuestion(prochaineQuestion);
     } else {
       afficherScoreFinal(); // Afficher le score final lorsque le quiz est terminé
@@ -97,15 +93,15 @@ function afficherScoreFinal() {
   reponsesElement.innerHTML = "";
 
   // Afficher le score final
-  var scoreText = "Votre score est de " + score + " sur " + quizData.questions.length + ".";
-  var scoreElement = document.createElement("p");
+  const scoreText = `Votre score est de ${score} sur ${quizData.questions.length}.`;
+  const scoreElement = document.createElement("p");
   scoreElement.textContent = scoreText;
   reponsesElement.appendChild(scoreElement);
 }
 
 // Charger le quiz lors du chargement de la page
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
   // Afficher la première question du quiz
-  var premiereQuestion = quizData.questions[questionIndex];
+  const premiereQuestion = quizData.questions[questionIndex];
   afficherQuestion(premiereQuestion);
 });
